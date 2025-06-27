8333f6d5511d89051914f92af19987ba576bba76
Jasper Drescher <jasperdre@gmail.com>
2025-06-27 16:54:19 +0900

    Create a custom player controller and a BP for the custom player state

---
+++ Tale/Source/Tale/TalePlayerController.cpp
@@ -0,0 +1,4 @@
+// Copyright (c) 2025 Jasper Drescher. All rights reserved.
+
+#include "TalePlayerController.h"
+
---
+++ Tale/Source/Tale/TalePlayerController.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2025 Jasper Drescher. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TalePlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TALE_API ATalePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
